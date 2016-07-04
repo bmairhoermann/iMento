3d9dded872decd60fc706f846e8c953ce38a3e18
Brian Mairhörmann <brian.mairhoermann@hof-university.de>
2016-07-04 14:46:07 +0200

    BackButton + Aufräumarbeiten

--- imento/imento/MainPage.xaml.cs
+++ imento/imento/MainPage.xaml.cs
@@ -31,8 +31,6 @@ namespace imento
             MainFrame.Navigate(typeof(HomeView));
             // Overwrite the default minimum size settings for the window
             Windows.UI.ViewManagement.ApplicationView.GetForCurrentView().SetPreferredMinSize(new Size(200, 100));
-            // Disable Back Button
-            // GoBack.Visibility = Visibility.Collapsed;
 
             // Handle the GoBack Button in the Title Bar or the Button on Windows Phones 
             var systemNavigationManager = SystemNavigationManager.GetForCurrentView();
@@ -47,27 +45,15 @@ namespace imento
 
         // ListBox_SelectionChanged: Changes view according to the pressed button 
         private void ListBox_SelectionChanged(object sender, SelectionChangedEventArgs e) {
-            // GoBack.Visibility = Visibility.Visible;
             if (Home.IsSelected) {
                 MainFrame.Navigate(typeof(HomeView));
                 Title.Text = "Startseite";
-                // GoBack.Visibility = Visibility.Collapsed;
-            /*
-            }else if (NewAlbum.IsSelected) {
-                MainFrame.Navigate(typeof(Views.EntryView)); // wtf...
-                Title.Text = "Neues Album anlegen";
-            */
             } else if (AllAlbums.IsSelected) {
                 MainFrame.Navigate(typeof(AllAlbumsView));
                 Title.Text = "Alle Alben";
             } else if (Favs.IsSelected) {
                 MainFrame.Navigate(typeof(FavsView));
                 Title.Text = "Meine Favoriten";
-            /*
-            } else if (Settings.IsSelected) {
-                MainFrame.Navigate(typeof(SettingsView));
-                Title.Text = "Einstellungen";
-            */
             } else if (Help.IsSelected){
                 MainFrame.Navigate(typeof(HelpView));
                 Title.Text = "Hilfe und Kurzanleitung";
@@ -76,28 +62,34 @@ namespace imento
                 Title.Text = "Über diese App";
             }
         }
-        /*
-        private void GoBack_Click(object sender, RoutedEventArgs e) {
-            if (MainFrame.CanGoBack) {
-                MainFrame.GoBack();
-                Title.Text = "Zurück gegangen";
-            } else {
-                GoBack.Visibility = Visibility.Collapsed;
-            }
-        }
-        */
 
         // Function for handling the GoBack Button
         private void SystemNavigationManagerOnBackRequested(object sender, BackRequestedEventArgs backRequestedEventArgs) {
             if (MainFrame.CanGoBack) {
-                MainFrame.GoBack();
-                backRequestedEventArgs.Handled = true;
-                try {
-                    Title.Text = "";
-                } catch (Exception e) {
-
+                var lastPageName = MainFrame.BackStack.Last().SourcePageType.Name;
+                if (lastPageName == "HomeView") {
+                    MainFrame.GoBack();
+                    Title.Text = "Startseite";
+                } else if (lastPageName == "AllAlbumsView") {
+                    MainFrame.GoBack();
+                    Title.Text = "Alle Alben";
+                } else if (lastPageName == "FavsView") {
+                    MainFrame.GoBack();
+                    Title.Text = "Meine Favoriten";
+                } else if (lastPageName == "HelpView") {
+                    MainFrame.GoBack();
+                    Title.Text = "Hilfe und Kurzanleitung";
+                } else if (lastPageName == "AboutView") {
+                    MainFrame.GoBack();
+                    Title.Text = "Über diese App";
+                } else if (lastPageName == "AlbumView") {
+                    MainFrame.GoBack();
+                    Title.Text = "Album";
+                } else if (lastPageName == "EntryView") {
+                    MainFrame.GoBack();
+                    Title.Text = "Eintrag";
                 }
-                
+                backRequestedEventArgs.Handled = true;                
             }
         }
     }
--- imento/imento/Models/Album.cs
+++ imento/imento/Models/Album.cs
@@ -6,16 +6,6 @@ using System.Threading.Tasks;
 
 namespace imento.Models {
     public class Album {
-        /*
-        public int AlbumId { get; set; }
-        public string AlbumTitle { get; set; }
-        public string AlbumDescription { get; set; }
-        public string HolidayType { get; set; }
-        public string Location { get; set; }
-        public string PicSource { get; set; }
-        // public int[] PictureIds;
-        */
-
         public string AlbumId { get; set; }
 
         public string Title { get; set; }
@@ -28,84 +18,5 @@ namespace imento.Models {
 
         public List<Entry> Entries { get; set; }
     }
-    /*
-    public class AlbumManager {
-        public static List<Album> GetAlbums() {
-            var albums = new List<Album>();
-            int[] pic = { 1, 2, 3 };
-
-            albums.Add(new Album {
-                AlbumId = 1,
-                AlbumTitle = "Barcelona",
-                AlbumDescription = "Beschreibungstext für ein Album",
-                HolidayType = "Strandurlaub",
-                Location = "Barcelona",
-                PicSource = "../Pictures/pic01.jpg"
-            });
-
-            albums.Add(new Album {
-                AlbumId = 2,
-                AlbumTitle = "Strand",
-                AlbumDescription = "Beschreibungstext für ein Album",
-                HolidayType = "Strandurlaub",
-                Location = "Barcelona",
-                PicSource = "../Pictures/pic02.jpg"
-            });
-
-            albums.Add(new Album {
-                AlbumId = 3,
-                AlbumTitle = "Wandern",
-                AlbumDescription = "Beschreibungstext für ein Album",
-                HolidayType = "Strandurlaub",
-                Location = "Barcelona",
-                PicSource = "../Pictures/pic03.jpg"
-            });
-            albums.Add(new Album {
-                AlbumId = 4,
-                AlbumTitle = "Zeuch",
-                AlbumDescription = "Beschreibungstext für ein Album",
-                HolidayType = "Strandurlaub",
-                Location = "Barcelona",
-                PicSource = "../Pictures/pic04.jpg"
-            });
-
-            albums.Add(new Album {
-                AlbumId = 5,
-                AlbumTitle = "asdf",
-                AlbumDescription = "Beschreibungstext für ein Album",
-                HolidayType = "Strandurlaub",
-                Location = "Barcelona",
-                PicSource = "../Pictures/pic05.jpg"
-            });
-            albums.Add(new Album {
-                AlbumId = 6,
-                AlbumTitle = "Test",
-                AlbumDescription = "Beschreibungstext für ein Album",
-                HolidayType = "Strandurlaub",
-                Location = "Barcelona",
-                PicSource = "../Pictures/pic06.jpg"
-            });
-
-            albums.Add(new Album {
-                AlbumId = 7,
-                AlbumTitle = "Test",
-                AlbumDescription = "Beschreibungstext für ein Album",
-                HolidayType = "Strandurlaub",
-                Location = "Barcelona",
-                PicSource = "../Pictures/pic07.jpg"
-            });
-            albums.Add(new Album {
-                AlbumId = 8,
-                AlbumTitle = "Test",
-                AlbumDescription = "Beschreibungstext für ein Album",
-                HolidayType = "Strandurlaub",
-                Location = "Barcelona",
-                PicSource = "../Pictures/pic08.jpg"
-            });
-
-
-
-            return albums; 
-        }*/
-    }
+}
 
